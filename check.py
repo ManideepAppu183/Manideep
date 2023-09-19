e58fbba20b3b3aa0bb7b820669d542e0ae68333e
ManideepAppu183 <113026061+ManideepAppu183@users.noreply.github.com>
2023-09-19 19:40:57 +0530

    Update check.py

--- check.py
+++ check.py
@@ -7,14 +7,44 @@
 # else:
 #     print(f"{number} is odd.")
 
-def add(x,y):
-    return x+y
+# def add(x,y):
+#     return x+y
 
-def subtract(x,y):
-    return x-y
+# def subtract(x,y):
+#     return x-y
 
-def multiply(x,y):
-    return x*y
+# def multiply(x,y):
+#     return x*y
 
-def divide(x,y):
-    return x/y
+# def divide(x,y):
+#     return x/y
+import math
+def sum(a,b):
+    return a + b
+
+def difference(a,b):
+    return a - b
+
+def product(a,b):
+    return a * b
+
+def quotient(a,b):
+    return a / b
+
+def power(a,b):
+    return a ** b
+
+def remainder(a,b):
+    return a % b
+
+def logarithm(a,b):
+    return math.log(a,b)
+
+def sine(a):
+    return math.sin(a)
+
+def cosine(a):
+    return math.cos(a)
+
+def tangent(a):
+    return math.tan(a)
