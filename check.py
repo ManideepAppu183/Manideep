39f88606708f46c39c8f4112984711e97bdf18a4
ManideepAppu183 <113026061+ManideepAppu183@users.noreply.github.com>
2023-09-19 08:13:59 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,8 +1,20 @@
-# Input a number from the user
-number = 5
+# # Input a number from the user
+# number = 5
 
-# Check if the number is even or odd
-if number % 2 == 0:
-    print(f"{number} is even.")
-else:
-    print(f"{number} is odd.")
+# # Check if the number is even or odd
+# if number % 2 == 0:
+#     print(f"{number} is even.")
+# else:
+#     print(f"{number} is odd.")
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
