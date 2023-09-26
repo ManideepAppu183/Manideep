fc7b8a34020524a649e2f99659b7085ac0989397
Manideep Ediga <113026061+ManideepAppu183@users.noreply.github.com>
2023-09-26 13:15:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,3 @@
-# from flask import Flask,request
-# from flask_restful import Api, Resource
-#
-# app = Flask(__name__)
-# api = Api(app)
-#
-# students = {
-#     1: {"name": "John Doe", "age": 20, "grade": "A"},
-#     2: {"name": "Jane Smith", "age": 22, "grade": "B"},
-#     3: {"name": "Bob Johnson", "age": 21, "grade": "C"},
-# }
-#
-# class Student(Resource):
-#     def get(self, student_id):
-#         return students[student_id]
-#
-#     def put(self, student_id):
-#         data = request.get_json()
-#         students[student_id] = data
-#         return {"message": "Student record updated", "student_id": student_id}
-#
-#
-# api.add_resource(Student, "/student/<int:student_id>")
-#
-# @app.route('/')
-# def home_page():
-#     return "Welcome to the Student API!"
-#
-# if __name__ == "__main__":
-#     app.run(debug=True, host='0.0.0.0', port=5000)
 
 from flask import Flask, request, render_template
 from flask_restful import Api, Resource
